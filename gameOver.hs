e932edecd08d7753250b7a960fc620abfddacf9a
abrarmusa <abrar.musa.89@gmail.com>
2015-12-02 17:15:41 -0800

    a

--- gameOver.hs
+++ gameOver.hs
@@ -4,18 +4,18 @@ gameOver board history n = boardSeen board history
 
 
 boardSeen :: Board -> [Board] -> Bool
-boardSeen current_board (b:boards) 
+boardSeen current_board boards 
     | boards == [] = false  -- if the board is empty return false
-    | current_board == b = true  --  if the current board is in list of boards return true
-    | otherwise = boardSeen (current_board) (boards)
+    | current_board == (head boards) = true  --  if the current board is in list of boards return true
+    | otherwise = boardSeen (current_board) (tail boards) -- else recursively call boardSeen with the rest of the history and the current board
 
 
 countPieces :: Board -> Int -> Int -> Int
 countPieces board whitecount blackcount n
-    | board == [] = if (whitecount < n) then true
-                    else if (blackcount < n) then true
-                    else false
-    | (head board) == "D" = countPieces (tail board) whitecount blackcount n
-    | (head board) == "W" = countPieces (tail board) (whitecount + 1) blackcount n
-    | (head board) == "B" = countPieces (tail board) whitecount (blackcount + 1) n
+    | board == [] = if (whitecount < n) then true  -- if the board is empty and count of white pieces < 0, return true
+                    else if (blackcount < n) then true -- else if count of black pieces < 0, return true
+                    else false -- return false
+    | (head board) == "D" = countPieces (tail board) whitecount blackcount n -- if the piece is "D", recursively call countPieces with the rest of the board
+    | (head board) == "W" = countPieces (tail board) (whitecount + 1) blackcount n -- if the piece is "W", recursively call countPieces with the rest of the board and increment the count of white pieces
+    | (head board) == "B" = countPieces (tail board) whitecount (blackcount + 1) n -- if the piece is "B", recursively call countPieces with the rest of the board and increment the count of black pieces
  
